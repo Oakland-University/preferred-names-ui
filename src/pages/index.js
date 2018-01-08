7b157c4148b9cd3f0ff5cb717521b1bd6669d984
Corey Rowe <coreyr1196@gmail.com>
2018-01-08 11:07:41 -0500

    Add table

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,12 @@ import Dialog, {
   DialogContentText,
   DialogActions,
 } from 'material-ui/Dialog';
+import Table, {
+  TableBody,
+  TableCell,
+  TableHead,
+  TableRow,
+} from 'material-ui/Table';
 import Typography from 'material-ui/Typography';
 import { withStyles } from 'material-ui/styles';
 import withRoot from '../withRoot';
@@ -17,6 +23,54 @@ const styles = theme => ({
     paddingTop: theme.spacing.unit * 20,
   },
 });
+
+let cellId = 0;
+function createData(studentId, prefNameOld, prefNameNew, date) {
+  cellId += 1;
+  return { cellId, studentId, prefNameOld, prefNameNew, date };
+}
+
+const data = [
+  createData('123', 'Michael', 'Mike', '01-01-2018'),
+  createData('456', 'Anderson', 'Andy', '01-01-2018'),
+  createData('789', 'James', 'Jim', '01-01-2018'),
+  createData('012', 'Pamela', 'Pam', '01-01-2018'),
+]
+
+function NamesTable(props) {
+  const { classes } = props;
+
+  return (
+    <Table>
+      <caption>Modified Preferred Names</caption>
+      <TableHead>
+        <TableRow>
+          <TableCell style={{color: 'black'}}>Student ID</TableCell>
+          <TableCell style={{color: 'black'}}>Old Preferred Name</TableCell>
+          <TableCell style={{color: 'black'}}>New Preferred Name</TableCell>
+          <TableCell style={{color: 'black'}}>Date Modified</TableCell>
+        </TableRow>
+      </TableHead>
+      <TableBody>
+        {data.map(n => {
+          return (
+            <TableRow key={n.cellId}>
+              <TableCell>{n.studentId}</TableCell>
+              <TableCell>{n.prefNameOld}</TableCell>
+              <TableCell><strong>{n.prefNameNew}</strong></TableCell>
+              <TableCell>{n.date}</TableCell>
+            </TableRow>
+          );
+        })}
+      </TableBody>
+    </Table>
+  );
+}
+
+NamesTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
 
 class Index extends React.Component {
   state = {
@@ -41,26 +95,7 @@ class Index extends React.Component {
 
     return (
       <div className={classes.root}>
-        <Dialog open={open} onClose={this.handleClose}>
-          <DialogTitle>Super Secret Password</DialogTitle>
-          <DialogContent>
-            <DialogContentText>1-2-3-4-5</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button color="primary" onClick={this.handleClose}>
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
-        <Typography type="display1" gutterBottom>
-          Material-UI
-        </Typography>
-        <Typography type="subheading" gutterBottom>
-          example project
-        </Typography>
-        <Button raised color="accent" onClick={this.handleClick}>
-          Super Secret Password
-        </Button>
+        <NamesTable />
       </div>
     );
   }
