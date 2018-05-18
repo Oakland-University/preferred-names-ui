893fa323fbbe216dba7e38100c0878d6c9ec0b65
KaJuan Johnson <kdjohnson23@oakland.edu>
2018-05-18 12:21:55 -0400

    Update material-ui to v1.

--- src/index.js
+++ src/index.js
@@ -2,21 +2,21 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PrefNames from './prefnames'
 
-import { MuiThemeProvider, createMuiTheme } from 'material-ui/styles'
+import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles'
 
 // Do not delete. This is required to authenticate the view in uPortal.
 /* global token */
 
 const theme = createMuiTheme({
-    typography: {
-        fontFamily: 'Roboto',
-        fontSize: '16px'
-    }
+  typography: {
+    fontFamily: 'Roboto',
+    fontSize: '16px'
+  }
 })
 
 ReactDOM.render(
-    <MuiThemeProvider theme={theme}>
-        <PrefNames token={token} />
-    </MuiThemeProvider>,
-    document.querySelector('#preferrednames-root')
+  <MuiThemeProvider theme={theme}>
+    <PrefNames token={token} />
+  </MuiThemeProvider>,
+  document.querySelector('#preferrednames-root')
 )
